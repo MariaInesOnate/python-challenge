7a3e188439ffde226674c4c9451eae667c82a0af
MariaInesOnate <79945107+MariaInesOnate@users.noreply.github.com>
2021-04-08 20:32:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import csv
 import os 
 
-filename = 'budget_data.csv'
+#PyBankExercise#
+#Create path to csv file# 
+csvpath = os.path.join("Resources", "budget_data.csv")
+
+#Set Variables#
+total_months = 0 
+total_revenue = 0 
+change_in_month = []
+list_profitloss = []
+prev_month = 0 
+
+#Open csv file#
+with open(csvpath, 'r') as budget: 
+  
