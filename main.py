5ca20d51e29a4d9c882b37796b8856ae0c6e6eef
MariaInesOnate <79945107+MariaInesOnate@users.noreply.github.com>
2021-04-08 20:40:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,18 @@ total_revenue = 0
 change_in_month = []
 list_profitloss = []
 prev_month = 0 
+max_decrease = revenue [0]
+max_increase = revenue [0]
+
 
 #Open csv file#
 with open(csvpath, 'r') as budget: 
-  
+
+#Now store content in csvreader#  
+        csvreader = csv.reader(budget, delimiter = ',') 
+        csv_header = next(csvreader) 
+#Time to iterate# 
+        
+
+#Total months# 
+total_months = len(months)
